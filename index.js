35b88d55b98ab8cd588fc8c79bd6779455ad8976
Cedric Lamy <lamycedric@gmail.com>
2015-08-13 16:24:07 -0700

    confusion number 2

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Routing
+app.use(express.static(__dirname + '/src'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/src/index.html');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
 });
