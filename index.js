b0737e38c84e66665580e7532dafa70c3fd40c8b
Cedric Lamy <lamycedric@gmail.com>
2015-08-17 16:19:32 -0700

    updating github for sharing

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 var express = require('express');
 var app = express();
 
+var server = require('http').createServer(app);
+
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(server);
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
 
 // Routing
 app.use(express.static(__dirname + '/src'));
 
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/src/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
+app.get('/login', function(req, res){
+  res.sendFile(__dirname + '/login.html');
+});
+
+io.listen(server);
 
 io.on('connection', function(socket){
   console.log('a user connected');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
+// var socket = io();
+// $('form').submit(function(){
+//   socket.emit('chat message', $('#m').val());
+//   $('#m').val('');
+//   return false;
+// });
+
 var socket = io();
+$('form').submit(function() {
+  socket.emit('chat message', $('#user_message').val());
+  $('#user_message').val('');
+  //Don't refresh the page
+  return false;
+});
+
+socket.on('chat message', function(msg) {
+  $('#messages').append($('<li>').text(msg));
+});
